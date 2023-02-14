4d9ea31c2e83fd9a43cf73edc4f341c6d5408330
valdiev <valdiev@live.fr>
2023-02-14 11:49:44 +0100

    Run build for Git folder

--- build/1-block/index.asset.php
+++ build/1-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '704a143b237a82e36023');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7cf6c2200add3d470b12');
--- build/2-block/index.asset.php
+++ build/2-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2fede15a2b2adced0534');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '73ed1394db7a6a541980');
